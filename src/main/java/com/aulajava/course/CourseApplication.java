c8e80b604d12d57d76fcf584fb4e550589a90382
Kauê Santos Filgueiras <kauefilgueiras@gmail.com>
2021-11-21 13:37:10 -0300

    Primeiro Commit

---
+++ src/main/java/com/aulajava/course/CourseApplication.java
@@ -0,0 +1,16 @@
+package com.aulajava.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages={"resources"})
+
+public class CourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseApplication.class, args);
+    }
+
+}
